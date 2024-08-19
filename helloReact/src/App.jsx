12e9142148823ee207173c18109b3a34aa2421b1
guilhxrme05 <gguihurtado@gmail.com>
2024-08-19 17:07:44 -0300

    Adicionado helloReact

---
+++ helloReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+   <h1 className='titulo'>Hello React JS fofinho</h1>
+   <p className='paragrafo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem rem possimus id exercitationem consequuntur quo alias non animi officia! Esse qui molestiae voluptates dolor explicabo voluptatem eveniet, sapiente saepe quisquam!</p>
+   
+    </>
+  )
+}
+
+export default App
